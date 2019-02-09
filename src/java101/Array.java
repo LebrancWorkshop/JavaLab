4c8755036a9b535571eae57fc12a2b38b19d2d4c
lebrancconvas <lebranccconvas@gmail.com>
2019-02-10 05:59:31 +0700

    :tada: Create Repository.

---
+++ src/java101/Array.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java101;
+
+/**
+ *
+ * @author User
+ */
+public class Array {
+    public static void main(String[] args) {
+        int[] Cyber = {1,2,3,4};
+        for(int i: Cyber){
+            System.out.println("Number " + i);
+        }
+    }
+}
